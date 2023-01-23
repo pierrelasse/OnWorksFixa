355f334efb84ef841c25dac597dc5c98021a0b41
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2023-01-23 16:50:28 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,13 +4,14 @@ set -e
 
 echo "* Welcom :D"
 echo "* IF THERE IS ERROR ABOUT 'Bla. It is held by process ???' THEN PRESS CTRL + C AND EXECUTE 'kill -9 <the code>' AND RUN THIS SCRIPT AGAIN!"
-
-apt update -y
-
-dpkg --configure -a
-
-apt remove unattended-upgrades -y
-
+echo ""
+echo "* Executing apt update -y"; apt update -y
+echo ""
+echo "* Executing dpkg --configure -a"; dpkg --configure -a
+echo ""
+echo "* Executing apt remove unattended-upgrades -y"; apt remove unattended-upgrades -y
+echo ""
+echo "* Uninstalling gnome apps"
 apt remove gnome-bluetooth -y
 apt remove gnome-calculator -y
 apt remove gnome-calendar -y
@@ -22,9 +23,9 @@ apt remove gnome-screenshot -y
 apt remove gnome-sudoku -y
 apt remove gnome-todo -y
 apt remove gnome-todo-common -y
-
-apt upgrade -y
-
-apt autoremove -y
+echo ""
+echo "* Executing apt upgrade -y"; apt upgrade -y
+echo ""
+echo "* Executing apt autoremove -y"; apt autoremove -y
 
 echo "* Done :D"
