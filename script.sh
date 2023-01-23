c19958f9aeda15a1ef277060d22fffe3f614268e
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2023-01-23 16:11:56 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo -e -n "Did you ran 'sudo -i' and typed in '123456'? (y/N): "
-read -r CONFIRM
-if [[ "$CONFIRM" =~ [Yy] ]]; then
-    execute "$2"
-else
-    error "Aborted."
-    exit 1
-fi
+echo "HIIIIIIIIIII!"
