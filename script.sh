fa3434a925bd555064396b6da509fd20b0a0d0dc
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2023-01-23 16:25:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ set -e
 echo "* Welcom :D"
 echo "* IF THERE IS ERROR ABOUT 'Bla. It is held by process ???' THEN PRESS CTRL + C AND EXECUTE 'kill -9 <the code>' AND RUN THIS SCRIPT AGAIN!"
 
-apt remove unattended-upgrades
 dpkg --configure -a
+apt remove unattended-upgrades
 apt update -y
 apt upgrade -y
 apt autoremove -y
