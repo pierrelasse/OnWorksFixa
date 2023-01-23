c15f1ee75cf87db6e2df735a10da88af8b1273a1
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2023-01-23 16:28:48 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,24 @@ echo "* Welcom :D"
 echo "* IF THERE IS ERROR ABOUT 'Bla. It is held by process ???' THEN PRESS CTRL + C AND EXECUTE 'kill -9 <the code>' AND RUN THIS SCRIPT AGAIN!"
 
 dpkg --configure -a
-apt remove unattended-upgrades
+
+apt remove unattended-upgrades -y
+
+apt remove gnome-bluetooth -y
+apt remove gnome-calculator -y
+apt remove gnome-calendar -y
+apt remove gnome-characters -y
+apt remove gnome-getting-started-docs -y
+apt remove gnome-mahjongg -y
+apt remove gnome-mines -y
+apt remove gnome-screenshot -y
+apt remove gnome-sudoku -y
+apt remove gnome-todo -y
+apt remove gnome-todo-common -y
+
 apt update -y
 apt upgrade -y
+
 apt autoremove -y
 
 echo "* Done :D"
